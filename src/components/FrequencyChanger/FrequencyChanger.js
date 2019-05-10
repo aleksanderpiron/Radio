08c07dace911e57881344f84ca6708cda1705083
Aleksander <pironaleksander@gmail.com>
2019-05-10 14:54:35 +0200

    Update FrequencyChanger.js

---
+++ src/components/FrequencyChanger/FrequencyChanger.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import plusIcon from '../../imgs/plus.png';
+import minusIcon from '../../imgs/minus.png';
+
+const FrequencyChanger = (props) =>{
+    return(
+        <div className="frequencyChanger">
+            <button></button>
+            {/* <span><img src="." alt=""/></span> */}
+            <button></button>
+        </div>
+    )
+}
+
+export default FrequencyChanger;

70953ce946a6f3bd532c12fc00b227f5e5667956
aleksanderpiron1 <pironaleksander@gmail.com>
2019-05-22 19:27:36 +0200

    Updates

---
+++ precache-manifest.e55f53f1228cc96d54ae549c4bf8d203.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ac0b04f427694259b56d12c7431108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76bee6d0eb090983ebf2",
+    "url": "/static/css/main.23f98d0f.chunk.css"
+  },
+  {
+    "revision": "e19398f8de4858ed13e4",
+    "url": "/static/js/2.aa3d1e0c.chunk.js"
+  },
+  {
+    "revision": "76bee6d0eb090983ebf2",
+    "url": "/static/js/main.d80ff16a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3d0ff30da735b20aeb90da54af3cfcad",
+    "url": "/static/media/radioImg1.3d0ff30d.png"
+  },
+  {
+    "revision": "af003bf04b3a03ef1b72ed62ef2a2554",
+    "url": "/static/media/radioImg2.af003bf0.jpg"
+  },
+  {
+    "revision": "0c16267d5742a0ee6e78a2dab563a2a0",
+    "url": "/static/media/radioImg3.0c16267d.jpg"
+  },
+  {
+    "revision": "3a0c76ad0e075df488bc636898762f25",
+    "url": "/static/media/switch.3a0c76ad.png"
+  }
+]);
